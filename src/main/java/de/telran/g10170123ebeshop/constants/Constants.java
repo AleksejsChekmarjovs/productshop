3d1b7a8b50d4f5c8dfc6c3b28b1332019c50bafc
Aleksejs <dyugary@gmail.com>
2023-09-28 11:38:59 +0300

    just copied

---
+++ src/main/java/de/telran/g10170123ebeshop/constants/Constants.java
@@ -0,0 +1,13 @@
+package de.telran.g10170123ebeshop.constants;
+
+public class Constants {
+    String DB_DRIVER_PATH = "com.mysql.cj.jdbc.Driver";
+    String DB_ADDRESS = "jdbc:mysql://localhost:3306/";
+    String DB_NAME = "shop_10-170123-e-be";
+    String DB_USER_NAME = "root";
+    String DB_PASSWORD = "55555";
+
+    String ID = "id";
+    String NAME = "name";
+    String PRICE = "price";
+}
